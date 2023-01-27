824538cfe5376f57c2ce7af8d3812673a58f5126
StefyfreshSchool <90330171+StefyfreshSchool@users.noreply.github.com>
2023-01-27 08:37:11 -0500

    Vue stuffs

---
+++ Vue JS/task-tracker-app/src/components/Header.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+        <Button @btn-click="$emit('toggle-form')" :text="isActive ? 'Close' : 'Add Task'" :color="isActive ? 'red' : 'green'"></Button>
+    </header>
+</template>
+
+<script>
+import Button from '../components/Button.vue'
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Header',
+    props: {
+        title: String,
+        isActive: Boolean,
+    },
+    components: {
+        Button
+    }
+    
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ Vue JS/task-tracker-app/src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div :key="task.id" v-for="task in tasks">
+            <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task"></Task>
+        </div>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    }
+}
+</script>
